82bb0f3e6c2aa6a7caa8c883be0e57f0c8f7efd7
Germán Martinez <germand_m@hotmail.com>
2022-10-11 12:10:21 -0300

    sync

---
+++ clase_04_Generaciones_digitales.py
@@ -0,0 +1,18 @@
+'''
+- Generación Silenciosa 1920-1940
+- Generación Baby Boomer 1946-1964
+- Generación X 1965-1979
+- Generación Y 1980-2000
+- Generación Z 2001-2010
+'''
+
+año = 123
+
+if (año >= 1920 and año <= 1940):                                              
+    print("Gen Silenciosa")
+elif año >= 1946 and año <=1964:
+    print("Gen Baby Boomer")
+# Completar con los que falta
+else:
+    print("Sin clasificar")
+    print("123")
