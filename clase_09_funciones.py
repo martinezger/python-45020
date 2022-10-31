37d48fdcae048adb755058f4f854bd6a5e8b1f27
Germán Martinez <germand_m@hotmail.com>
2022-10-31 18:42:56 -0300

    clase func

---
+++ clase_09_funciones.py
@@ -0,0 +1,98 @@
+def imprimir_por_pantalla_archivo_chat():
+    print("------------INICIO---------------")
+    archivo_texto = open("chat.txt", "r")
+    for linea_de_texto in archivo_texto:
+        print(linea_de_texto)
+    archivo_texto.close() 
+    print("------------FIN---------------")
+
+
+archivo_texto = open("chat.txt", "w")
+
+contenido = """Claudio: Hola german como estas?
+German: Todo bien, vos?.
+Claudio: laksdjlakjsdlkajsd
+German: alskdjlaksjdlkajsd
+Ruben: laksjdlakjsdlkjas
+"""
+
+archivo_texto.write(contenido)
+archivo_texto.close()
+
+
+archivo_texto = open("chat.txt", "r")
+
+linea_1 = archivo_texto.readline()
+linea_2 = archivo_texto.readline()
+
+print(linea_1)
+print(linea_2)
+archivo_texto.readline()
+
+archivo_texto.close()
+
+imprimir_por_pantalla_archivo_chat()
+
+
+archivo_texto = open("chat.txt", "r")
+imprimir_por_pantalla_archivo_chat()
+
+
+
+archivo_texto = open("chat.txt", "r")
+
+archivo_texto.seek(20)
+
+imprimir_por_pantalla_archivo_chat()
+
+
+
+archivo_texto = open("chat.txt", "r")
+
+archivo_texto.seek(20)
+
+for linea in archivo_texto:
+  print(linea[10:])
+
+archivo_texto.close()
+
+
+
+archivo_texto = open("chat.txt", "a")
+
+contenido = "Claudio: Re bien!!\n"
+
+archivo_texto.write(contenido)
+
+archivo_texto.close()
+
+imprimir_por_pantalla_archivo_chat()
+
+
+archivo_texto = open("chat.txt", "a")
+
+contenido = "Romina: Re bien!!\n"
+
+archivo_texto.write(contenido)
+
+archivo_texto.close()
+
+
+imprimir_por_pantalla_archivo_chat()
+
+
+
+archivo_texto = open("chat.txt", "r")
+contenido = archivo_texto.readlines()
+archivo_texto.close()
+
+nuevo_contenido = []
+for linea in contenido:
+    nuevo_contenido.append(linea.replace("German", "Daniel"))
+    print(linea)
+    
+archivo_texto = open("chat.txt", "w")
+archivo_texto.writelines(nuevo_contenido)
+archivo_texto.close()
+
+imprimir_por_pantalla_archivo_chat()
